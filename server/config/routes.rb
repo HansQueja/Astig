62438d039188d6e7510363741dcada6fabefb908
Hans Queja <hansqueja8@gmail.com>
2025-02-07 15:28:43 +0800

    Feat: Setup initial routes for testing.

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -10,5 +10,15 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root "dashboards#index"
+
+  scope :api do
+    resources :dashboards, only: [:index] do
+    
+    end
+    
+    resources :users do
+    end
+
+  end
 end
