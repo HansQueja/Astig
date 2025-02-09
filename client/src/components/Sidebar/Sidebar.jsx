58c8c6590834a373a46ba9cb7fdfe4ff4077ba80
Hans Queja <hansqueja8@gmail.com>
2025-02-09 22:40:35 +0800

    Feat: Add initial dashboard and sidebar component configuration.

---
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,20 @@
+import './Sidebar.css'
+
+function Sidebar() {
+
+    return (
+        <div className="sidebar-container">
+            <div className="sidebar-margin">
+                <div className="astig-logo">
+                    ASTIG
+                </div>
+                <div className="astig-tagline">
+                    Alamin, Tuklasin!
+                </div>
+            </div>
+        </div>
+    );
+  
+}
+
+export default Sidebar
---
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import './Dashboard.css'
+
+function Dashboard() {
+    const [data, setData] = useState([]);
+
+    const getData = async () => {
+        try {
+            const response = await fetch(
+                'http://127.0.0.1:3000/api/dashboards',
+                {
+                    method: 'GET',
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+
+            if (response.ok) {
+                const result = await response.json();
+
+                console.log(result);
+                setData(result);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    const listItems = data.map(course =>
+        <li key={course.ID}>
+            {course.Course}
+        </li>
+    )
+
+    useEffect(() => {
+        getData();
+    }, [])
+
+    return (
+        <div>
+            <Sidebar />
+            {listItems}
+        </div>
+    );
+  
+}
+
+export default Dashboard
