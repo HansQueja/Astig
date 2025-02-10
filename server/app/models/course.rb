54925d07b16723f03efb9b403ea2e2b7aab3db61
Hans Queja <hansqueja8@gmail.com>
2025-02-10 15:50:15 +0800

    Refactor: Joins department and university tables for faster lookup.

--- server/app/models/course.rb
+++ server/app/models/course.rb
@@ -4,17 +4,15 @@ class Course < ApplicationRecord
 
     # Returns the complete details for a course
     def self.dashboard_data
-        raw_data = Course.all
+        raw_data = Course.left_joins(:university).left_joins(:department)
 
         data = []
 
         raw_data.each do |course|
             temp_data = {}
-            university = University.find(course.university_id)
-            department = Department.find(course.department_id)
             temp_data["Course"] = course.course_name
-            temp_data["University"] = university.name
-            temp_data["Department"] = department.name
+            temp_data["University"] = course.university.name
+            temp_data["Department"] = course.department.name
             temp_data["ID"] = course.id 
             data.push(temp_data)
         end
