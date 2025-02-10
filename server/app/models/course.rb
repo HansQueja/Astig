802d74a5501c088860942da7e601bb6f193cc1c5
Hans Queja <hansqueja8@gmail.com>
2025-02-10 16:27:00 +0800

    Refactor: Add pagination in the first query.

--- server/app/models/course.rb
+++ server/app/models/course.rb
@@ -3,18 +3,16 @@ class Course < ApplicationRecord
     belongs_to :department
 
     # Returns the complete details for a course
-    def self.dashboard_data
-        raw_data = Course.left_joins(:university).left_joins(:department)
+    def self.dashboard_data(page = 1)
+        raw_data = Course.left_joins(:university).left_joins(:department).page(page)
 
-        data = []
-
-        raw_data.each do |course|
-            temp_data = {}
-            temp_data["Course"] = course.course_name
-            temp_data["University"] = course.university.name
-            temp_data["Department"] = course.department.name
-            temp_data["ID"] = course.id 
-            data.push(temp_data)
+        data = raw_data.map do |course|
+            {
+                "Course" => course.course_name,
+                "University" => course.university.name,
+                "Department" => course.department.name,
+                "ID" => course.id
+            }
         end
 
         return data.as_json
