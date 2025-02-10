bff0bfe130f3c9e291a27ee5d88fb7aacbb1c54f
Hans Queja <hansqueja8@gmail.com>
2025-02-10 15:49:42 +0800

    Feat: Add pagination details in the json response.

--- server/app/controllers/dashboards_controller.rb
+++ server/app/controllers/dashboards_controller.rb
@@ -1,7 +1,18 @@
 class DashboardsController < ApplicationController
     def index
         data = Course.dashboard_data
+        pagination_data = Course.all.page(params[:page])
 
-        render json: data, status: :ok
+        render json: {
+            data: data,
+            pagination: {
+                current_page: pagination_data.current_page,
+                per_page: pagination_data.limit_value,
+                total_pages: pagination_data.total_pages,
+                total_items: pagination_data.total_count,
+                next_page: pagination_data.next_page,
+                previous_page: pagination_data.prev_page
+            }
+        }, status: :ok
     end
 end
