43a9eda529565b6e4634beb329f8259553c633ab
Hans Queja <hansqueja8@gmail.com>
2025-03-09 16:04:12 +0800

    Refactor: Remove backend pagination from model and controller methods.

--- server/app/models/course.rb
+++ server/app/models/course.rb
@@ -3,13 +3,13 @@ class Course < ApplicationRecord
     belongs_to :department
 
     # Returns the complete details for a course
-    def self.dashboard_data(page)
-        raw_data = Course.left_joins(:university).left_joins(:department).page(page)
+    def self.dashboard_data
+        raw_data = Course.left_joins(:university).left_joins(:department)
 
         data = raw_data.map do |course|
             {
-                "Course" => course.course_name,
-                "University" => course.university.name,
+                "Program" => course.course_name,
+                "Institution" => course.university.name,
                 "Department" => course.department.name,
                 "ID" => course.id
             }
